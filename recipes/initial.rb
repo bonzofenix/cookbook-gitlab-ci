f827265895a82a067ca173307a032774f725bb71
bonzofenix <bonzofenix@gmail.com>
2013-12-10 14:31:24 -0300

    remove redundant build essentials

--- recipes/initial.rb
+++ recipes/initial.rb
@@ -4,7 +4,6 @@ include_recipe "build-essential"
 include_recipe "git"
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
-include_recipe "build-essential"
 include_recipe "postgresql::server"
 
 
