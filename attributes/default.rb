1b980c69eebb859ef8b12a428c196e51d2e54801
Alan Moran <bonzofenix@gmail.com>
2014-04-21 10:25:26 -0700

    Git url via https

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['gitlab_ci']['app_home'] = "/home/gitlab_ci/gitlab-ci"
-default['gitlab_ci']['git']['url'] = "git://github.com/gitlabhq/gitlab-ci.git"
+default['gitlab_ci']['git']['url'] = "https://github.com/gitlabhq/gitlab-ci.git"
 default['gitlab_ci']['git']['branch'] = "4-0-stable"
 default['gitlab_ci']['user'] = "gitlab_ci"
 default['gitlab_ci']['home'] = "/home/gitlab_ci"
